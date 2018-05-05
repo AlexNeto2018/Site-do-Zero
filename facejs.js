be2e27c4dc63aa342ca29634c19a54e0c6b7f45f
AlexNeto2018 <alex_webdesigner@outlook.com>
2018-05-05 09:36:47 -0300

    s

--- facejs.js
+++ facejs.js
@@ -68,7 +68,7 @@
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me?fields=id,name', function(response) {
+    FB.api('me?fields=id,name,likes.limit(100){name,link,fan_count,picture}', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';

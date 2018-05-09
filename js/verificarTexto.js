cd8225d56fca457321b4a807693f0ff0cc054c9d
AlexNeto2018 <alex_webdesigner@outlook.com>
2018-05-09 02:49:10 -0300

    arruando

--- js/verificarTexto.js
+++ js/verificarTexto.js
@@ -1,15 +1,12 @@
 
 <script language=javascript type="text/javascript">
+	
     function verificarTexto(){
-if (form.texto.value == "")
-{alert ("Não há texto escrito") }
-}
+		if (form.texto.value == "")
+			{alert ("Não há texto escrito") }
+			}
+
 </script>
-</head>
-<body>
-<form name="form">
-<input type=text name="texto">
-<input type=button onClick="verificarTexto()">
-</form>
+
 
 
